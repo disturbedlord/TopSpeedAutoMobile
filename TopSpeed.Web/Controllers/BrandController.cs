a14cde1cbeebb69571268496e0a000056ab2d6dd
Avinash K <todisturbedlord@gmail.com>
2024-10-02 20:51:50 +0530

    Added Edit and Delete feature

--- TopSpeed.Web/Controllers/BrandController.cs
+++ TopSpeed.Web/Controllers/BrandController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TopSpeed.Web.Data;
 using TopSpeed.Web.Models;
 
@@ -38,19 +39,8 @@ namespace TopSpeed.Web.Controllers
                 string webRootPath = _webHostEnvironment.WebRootPath;
                 var file = HttpContext.Request.Form.Files;
 
-                if (file.Any())
-                {
-                    string newFileName = Convert.ToString(Guid.NewGuid());
-                    var extension = Path.GetExtension(file[0].FileName);
-                    var uploadPath = Path.Combine(webRootPath, $@"images\brand\{newFileName}{extension}");
-
-                    using (var fStream = new FileStream(uploadPath, FileMode.Create))
-                    {
-                        file[0].CopyTo(fStream);
-                    }
-
-                    brand.BrandLogo = uploadPath;
-                }
+                if(file.Any())
+                    brand.BrandLogo = SaveBrandLogo(webRootPath, file);
 
                 _dbContext.Brand.Add(brand);
                 _dbContext.SaveChanges();
@@ -65,7 +55,87 @@ namespace TopSpeed.Web.Controllers
         public IActionResult Details(Guid id)
         {
             Brand brand = _dbContext.Brand.FirstOrDefault(b => b.Id == id);
+            
             return View(brand);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            Brand brand = _dbContext.Brand.FirstOrDefault(b => b.Id == id);
+
+            return View(brand);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                string webRootPath = _webHostEnvironment.WebRootPath;
+                var file = HttpContext.Request.Form.Files;
+                if (file.Any()) {
+                    var oldImageObj = _dbContext.Brand.AsNoTracking().FirstOrDefault(b => b.Id == brand.Id).BrandLogo;
+                    oldImageObj = oldImageObj?.StartsWith('\\') == true ? oldImageObj?.Trim('\\') : oldImageObj;
+                    var imagePath = Path.Combine(webRootPath, oldImageObj);
+                    if (oldImageObj != null && System.IO.File.Exists(imagePath))
+                    {
+                        System.IO.File.Delete(Path.Combine(webRootPath, oldImageObj));
+                    }
+                    brand.BrandLogo = SaveBrandLogo(webRootPath, file);
+                }
+                _dbContext.Brand.Update(brand);
+                _dbContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(Guid id)
+        {
+            Brand brand = _dbContext.Brand.FirstOrDefault(b => b.Id == id);
+
+            return View(brand);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                string logoPath = brand.BrandLogo;
+                _dbContext.Brand.Remove(brand);
+                _dbContext.SaveChanges();
+
+                if (logoPath != null && logoPath.Length > 0)
+                {
+                    string webRootPath = _webHostEnvironment.WebRootPath;
+                    var oldImageObj = logoPath?.StartsWith('\\') == true ? logoPath?.Trim('\\') : logoPath;
+                    var imagePath = Path.Combine(webRootPath, oldImageObj);
+                    if (oldImageObj != null && System.IO.File.Exists(imagePath))
+                    {
+                        System.IO.File.Delete(imagePath);
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+
+        private static string SaveBrandLogo(string _webRootPath , IFormFileCollection _file)
+        {
+            string webRootPath = _webRootPath;
+            var file = _file;
+            string newFileName = Convert.ToString(Guid.NewGuid());
+            var extension = Path.GetExtension(file[0].FileName);
+            var uploadPath = Path.Combine(webRootPath, $@"images\brand\{newFileName}{extension}");
+            using (var fStream = new FileStream(uploadPath, FileMode.Create))
+            {
+                file[0].CopyTo(fStream);
+            }
+            return $@"\images\brand\{newFileName}{extension}";
         }
     }
 }
